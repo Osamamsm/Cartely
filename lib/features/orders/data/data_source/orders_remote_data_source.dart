cac62c99afb8934a662970808fc75ec71ffae8e9
Osama Selim <osamaselim216@gmail.com>
2026-08-08 19:44:48 +0300

    refactor: update getOrderDetailsById method to use named parameter and implement functionality

--- lib/features/orders/data/data_source/orders_remote_data_source.dart
+++ lib/features/orders/data/data_source/orders_remote_data_source.dart
@@ -5,25 +5,33 @@ import 'package:injectable/injectable.dart';
 
 abstract class OrdersRemoteDataSource {
   Future<List<OrderModel>> getOrders();
-  Future<OrderDetailsModel> getOrderDetailsById(String id);
+  Future<OrderDetailsModel> getOrderDetailsById({required String orderId});
 }
 
 @LazySingleton(as: OrdersRemoteDataSource)
 class OrdersRemoteDataSourceImpl implements OrdersRemoteDataSource {
-
   final SupabaseService _supabaseService;
 
   OrdersRemoteDataSourceImpl(this._supabaseService);
 
   @override
-  Future<OrderDetailsModel> getOrderDetailsById(String id) {
-    // TODO: implement getOrderDetailsById
-    throw UnimplementedError();
+  Future<OrderDetailsModel> getOrderDetailsById({required String orderId}) async {
+    final response = await _supabaseService.rpc(
+      function: 'get_order_by_id',
+      params: {'p_order_id': orderId},
+    );
+    final orderDetails = OrderDetailsModel.fromJson(
+      response as Map<String, dynamic>,
+    );
+    return orderDetails;
   }
 
   @override
-  Future<List<OrderModel>> getOrders() {
-    // TODO: implement getOrders
-    throw UnimplementedError();
+  Future<List<OrderModel>> getOrders() async {
+    final response = await _supabaseService.rpc(function: 'get_user_orders');
+    final List<OrderModel> orders = (response as List<dynamic>)
+        .map((order) => OrderModel.fromJson(order as Map<String, dynamic>))
+        .toList();
+    return orders;
   }
 }
