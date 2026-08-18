fc636d8acd9af54dd25c199fee44105e3e14080f
Osama Selim <osamaselim216@gmail.com>
2026-08-18 23:51:45 +0300

    refactor: update checkout view to use BlocConsumer for order status handling and launch payment URL

--- lib/features/checkout/presentation/widgets/checkout_view_body.dart
+++ lib/features/checkout/presentation/widgets/checkout_view_body.dart
@@ -5,20 +5,33 @@ import 'package:e_commerce/features/checkout/presentation/widgets/checkout_form_
 import 'package:e_commerce/features/checkout/presentation/widgets/checkout_result_body.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class CheckoutViewBody extends StatelessWidget {
   const CheckoutViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<CheckoutCubit, CheckoutState>(
+    return BlocConsumer<CheckoutCubit, CheckoutState>(
+      listenWhen: (previous, current) =>
+          previous.orderStatus != current.orderStatus,
+      listener: (context, state) async {
+        if (state.orderStatus == OrderStatus.awaitingPayment) {
+          final url = Uri.parse(state.placedOrder!.checkoutUrl!);
+          if (!await launchUrl(url)) {
+            throw Exception('Could not launch $url');
+          }
+        }
+        if (state.orderStatus == OrderStatus.orderPlaced) {
+          if (!context.mounted) return;
+          context.read<CartCubit>().resetCart();
+        }
+      },
       builder: (context, state) {
         if (state.orderStatus == OrderStatus.initial) {
-          return CheckoutFormBody();
-        } else {
-          context.read<CartCubit>().resetCart();
-          return CheckoutResultBody();
+          return const CheckoutFormBody();
         }
+        return const CheckoutResultBody();
       },
     );
   }
