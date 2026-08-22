d6b17326416d2e4f071a1d2df5a82eb8fdb4e9f0
Osama Selim <osamaselim216@gmail.com>
2026-08-22 23:06:41 +0300

    feat: implement reviews remote data source methods using Supabase service RPCs

--- lib/features/reviews/data/data_source/reviews_remote_data_source.dart
+++ lib/features/reviews/data/data_source/reviews_remote_data_source.dart
@@ -1,25 +1,26 @@
+import 'package:e_commerce/core/supabase/supabase_service.dart';
 import 'package:e_commerce/features/reviews/data/models/product_review_model.dart';
 import 'package:e_commerce/features/reviews/data/models/reviewable_product_model.dart';
 import 'package:e_commerce/features/reviews/data/models/user_review_model.dart';
+import 'package:e_commerce/features/reviews/data/review_operation_result_model.dart';
+import 'package:injectable/injectable.dart';
 
 abstract class ReviewsRemoteDataSource {
-  Future<void> addReview({
+  Future<ReviewOperationResultModel> addReview({
     required String productId,
     required String comment,
     required double rating,
   });
 
-  Future<void> editReview({
+  Future<ReviewOperationResultModel> editReview({
     required String reviewId,
     required String comment,
     required double rating,
   });
 
-  Future<void> deleteReview({required String reviewId});
+  Future<ReviewOperationResultModel> deleteReview({required String reviewId});
 
-  Future<List<UserReviewModel>> getUserReviews({
-    required int page,
-  });
+  Future<List<UserReviewModel>> getUserReviews({required int page});
 
   Future<List<ProductReviewModel>> getProductReviews({
     required String productId,
@@ -32,3 +33,97 @@ abstract class ReviewsRemoteDataSource {
   });
 }
 
+@LazySingleton(as: ReviewsRemoteDataSource)
+class ReviewsRemoteDataSourceImpl implements ReviewsRemoteDataSource {
+  final SupabaseService _supabaseService;
+
+  ReviewsRemoteDataSourceImpl(this._supabaseService);
+  @override
+  Future<ReviewOperationResultModel> addReview({
+    required String productId,
+    required String comment,
+    required double rating,
+  }) async {
+    final response = await _supabaseService.rpc(
+      function: 'add_review',
+      params: {
+        'p_product_id': productId,
+        'p_comment': comment,
+        'p_rating': rating,
+      },
+    );
+
+    return ReviewOperationResultModel.fromJson(response.data);
+  }
+
+  @override
+  Future<ReviewOperationResultModel> deleteReview({
+    required String reviewId,
+  }) async {
+    final response = await _supabaseService.rpc(
+      function: 'delete_review',
+      params: {'p_review_id': reviewId},
+    );
+    return ReviewOperationResultModel.fromJson(response.data);
+  }
+
+  @override
+  Future<ReviewOperationResultModel> editReview({
+    required String reviewId,
+    required String comment,
+    required double rating,
+  }) async {
+    final response = await _supabaseService.rpc(
+      function: 'edit_review',
+      params: {
+        'p_review_id': reviewId,
+        'p_comment': comment,
+        'p_rating': rating,
+      },
+    );
+    return ReviewOperationResultModel.fromJson(response.data);
+  }
+
+  @override
+  Future<List<ProductReviewModel>> getProductReviews({
+    required String productId,
+    double? ratingFilter,
+    required int page,
+  }) async {
+    final response = await _supabaseService.rpc(
+      function: 'get_product_reviews',
+      params: {
+        'p_product_id': productId,
+        'p_rating_filter': ratingFilter,
+        'p_page': page,
+      },
+    );
+    return (response.data as List)
+        .map((e) => ProductReviewModel.fromJson(e))
+        .toList();
+  }
+
+  @override
+  Future<List<ReviewableProductModel>> getReviewableProducts({
+    required int page,
+  }) async {
+    final response = await _supabaseService.rpc(
+      function: 'get_reviewable_products',
+      params: {'p_page': page},
+    );
+    return (response.data as List)
+        .map((e) => ReviewableProductModel.fromJson(e))
+        .toList();
+  }
+
+  @override
+  Future<List<UserReviewModel>> getUserReviews({required int page}) async {
+    final response = await _supabaseService.rpc(
+      function: 'get_user_reviews',
+      params: {'p_page': page},
+    );
+    return (response.data as List)
+        .map((e) => UserReviewModel.fromJson(e))
+        .toList();
+  }
+}
