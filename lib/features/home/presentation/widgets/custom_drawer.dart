d62396c4ba41540736603148bf9560e8f65a5b4a
Osama Selim <osamaselim216@gmail.com>
2026-08-24 13:41:58 +0300

    change app logo using flutter launcher icons package

--- lib/features/home/presentation/widgets/custom_drawer.dart
+++ lib/features/home/presentation/widgets/custom_drawer.dart
@@ -88,28 +88,14 @@ class _CustomDrawerFooter extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    return Container(
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        border: Border(
-          top: BorderSide(
-            color: theme.colorScheme.onSurface.withValues(alpha: 0.1),
-            width: 1,
-          ),
-        ),
+    return Image(
+      image: AssetImage(
+        Theme.of(context).brightness == Brightness.light
+            ? "assets/logo/dark_on_light_logo.png"
+            : "assets/logo/light_on_dark_logo.png",
       ),
-      child: Row(
-        children: [
-          Icon(
-            Icons.shopping_bag_outlined,
-            size: 20,
-            color: theme.colorScheme.primary,
-          ),
-          hGap(8),
-          Text(S.of(context).app_name, style: theme.textTheme.bodySmall),
-        ],
-      ),
+      height: 65,
+      width: 65,
     );
   }
 }
